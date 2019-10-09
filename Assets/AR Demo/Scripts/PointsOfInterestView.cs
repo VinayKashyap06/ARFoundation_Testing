7eced3dbc3ceca06c121c6bc87f1c2b398e33202
Vinay Kashyap <vinaykashyap57@gmail.com>
2019-10-09 16:37:01 +0530

    Added basic mapbox setup

---
+++ Assets/AR Demo/Scripts/PointsOfInterestView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Collider))]
+public class PointsOfInterestView : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+}
